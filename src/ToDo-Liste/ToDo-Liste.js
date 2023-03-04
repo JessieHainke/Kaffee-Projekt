6990aff74250207899b68c905c4b95ea31c7e753
Jessica Hainke <j-k.hainke@t-online.de>
2023-03-04 06:26:43 +0100

    An der ToDo-Liste herumgeschraubt, viel kommentiert, keine Fortschritte

--- src/ToDo-Liste/ToDo-Liste.js
+++ src/ToDo-Liste/ToDo-Liste.js
@@ -5,6 +5,8 @@ function bspFkt() {
     console.log(x);
 } */
 
+const { list } = require("postcss"); // was macht das hier? Habe ich das hier hinzugefügt? Der Code funktioniert ohne nicht, aber warum habe ich das da hin geschrieben?
+
 function ulliAdd() {
     
     var x = document.querySelector(".textfeld").value; // sammelt das ToDo aus dem Input-Feld
@@ -17,7 +19,6 @@ function ulliAdd() {
     
 }
 
-//document.querySelector(".addToDo").addEventListener("click", ulliAdd);
 
 
 
@@ -25,8 +26,8 @@ function ulliAdd() {
 
 function ulliRemove() {
 
-    const ToDoItems = document.querySelectorAll('li');
-    console.log(ToDoItems);
+    const ToDoItems = document.querySelectorAll('li'); // damit kann ich auf BESTIMMTE Listenitems zugreifen! Ich muss diese aber mit [Zahl, z.B. 2] explizit anpeilen
+    console.log(ToDoItems[2]);
 
     var tdlInhalt = document.querySelector(".toDoListe").value; // sammelt das Erledigte aus dem Input-Feld --> NEIN, sammelt NUR TO-DO-LISTEN-INHALT!!!
     console.log(tdlInhalt);
@@ -36,8 +37,26 @@ function ulliRemove() {
     
 
     document.querySelector(".toDoListe").remove(toDoDelete); // entfernt (im Moment alles) --> ist aber in der Liste?
+
     document.querySelector(".textRemove").value = ""; // leert Remove-Input-Feld :-)
     document.querySelector(".bg").style.backgroundColor = "white";
+
+    searchItem();
+
+    // ulliAdd();  // Idee: nochmaliges Aufrufen der Fkt ---> erneutes Aufrufen der Fkt funktioniert nicht, weil vorher alles von der ToDoListe gelöscht wird (?)
+
+    //ToDoItems.array.forEach(searchItem()); --> funktioniert nicht, kennt der "forEach" überhaupt?
+
+
+    function searchItem() {
+        if (tdlInhalt === toDoDelete) { // da ist die Verknüpfung falsch, aber auch sonst würde das so nicht funktionieren
+            delete toDoDelete;
+
+        } else {
+            console.log("ToDo ist nicht in der Liste"); // das zeigt er an - d.h. die Funktion und die if-else-Konstruktion funktionieren, aber das Abgleichen und Erkennen nicht
+        }
+    }
+
 }
 
 
@@ -50,13 +69,15 @@ function changeColor() {
     var b = Math.floor(Math.random() * '255');
     //var a = Math.random() * '1';
 
-    document.querySelector(".bg").style.backgroundColor = 'rgba(' + r + ',' + g + ',' + b + ')'; // damit es die Farbe wirklich nach rgb-Dings wechselt, müssen die Zahlen im richtigen Format sein (nm ng!)
+    document.querySelector(".bg").style.backgroundColor = 'rgb(' + r + ',' + g + ',' + b + ')'; // damit es die Farbe wirklich nach rgb-Dings wechselt, müssen die Zahlen im richtigen Format sein (nm ng!)
+
+
 }
 
 // TO DO FÜR DIE TO-DO-LISTE:
 // - Inputfeld leeren, wenn ToDo übernommen - ERLEDIGT :-)
 // - Inputfeld auch mit Enter-Taste bedienbar machen
 // - ToDo-Einträge löschen oder als erledigt markieren --> "remove" funktioniert nicht wirklich... --> vielleicht mit "delete"? 
-        //--> UM DA ERST EINMAL RAN ZU KOMMEN, KÖNNTE ICH AUS DEN ToDo-Items ein Array machen (bereits geschafft) und dann mit einer for-Schleife dieses durchsuchen! :-)
+        //--> UM DA ERST EINMAL RAN ZU KOMMEN, KÖNNTE ICH AUS DEN ToDo-Items ein Array machen (?) und dann mit einer for-Schleife dieses durchsuchen!
 
 // - FARBGENERATOR FUNKTIONIERT!! <3 :-)
