e44d340de3ca1e0efc84ece77c81a3a9841e2c94
Jessica Hainke <j-k.hainke@t-online.de>
2023-03-14 06:16:20 +0100

    Navigation funktioniert endlich, bei WeißAufSchwarz/WeißOhneSchwarz - Klassen ist irgendein Tippfehler (?)

--- src/js/menu_sp.js
+++ src/js/menu_sp.js
@@ -1,35 +1,19 @@
 
-document.querySelector(".animation").addEventListener ("click", animation, false);
+const navOverlay = document.querySelector('.nav');
+const navBtn = document.querySelector(".btn");
+const closeBtn = document.querySelector('.close')
 
-function animation() {                                  //     färbt die Hintergründe der divs schwarz und die Schrift weiß und zeigt den entsprechenden Text an (also hoffentlich irgendwann...)
 
-    var animationDiv = document.querySelector('.SmartphoneNav');
-   
-   if (animationDiv.style.display === "none") {
-        animationDiv.style.display = "block";
-        
-        
-    } else {
-        animationDiv.style.display = "none";
-        console.log('hier funktioniert es')
-    }
+
+navBtn.addEventListener ("click", openHandler);
+
+function openHandler() {
+    navOverlay.classList.remove("hidden");
 }
 
+closeBtn.addEventListener ("click", closeHandler)
 
-/*
+function closeHandler () {
+    navOverlay.classList.add("hidden");
+}
 
- var animationDiv = document.createElement("div"); // diese wunderbare Variable erstellt div-Elemente   
-
-    if (animationDiv.style.display === 'block') {
-        animationDiv.style.display = "none";
-    } else {
-        animationDiv.style.display = "block";
-    }
-
-    animationDiv.style.backgroundColor = "black";
-    animationDiv.style.color = "white";
-    animationDiv.style.height = "100vh";
-    animationDiv.style.width = "100vw";
-
-
-    */
